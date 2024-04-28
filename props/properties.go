6eb4be76938d0477df0ae2dff051c723072be1fb
Varshitha M R <harman.hm.m@mercedes-benz.com>
2024-04-28 12:36:50 +0530

    feat:Update properties and yaml file

--- props/properties.go
+++ props/properties.go
@@ -1,1 +1,12 @@
 package props
+
+type Properties struct {
+	Server ServerProps `yaml:"server"`
+}
+
+// ServerProps has the properties needed to create Server
+type ServerProps struct {
+	Host        string `yaml:"host"`
+	Port        int    `yaml:"port"`
+	ContextRoot string `yaml:"contextroot"`
+}
